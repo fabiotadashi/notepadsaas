609224d5394d8a79df0dadbc4d29e932ed04295c
logonrm <logonrm@fiap.com.br>
2017-08-15 20:52:50 -0300

    Busca por titulo

--- src/main/java/br/com/fiap/notepadsaas/controller/NotaController.java
+++ src/main/java/br/com/fiap/notepadsaas/controller/NotaController.java
@@ -2,16 +2,20 @@ package br.com.fiap.notepadsaas.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.notepadsaas.model.Nota;
 import br.com.fiap.notepadsaas.repository.NotaRepository;
 
 @RestController
+@CrossOrigin
+@RequestMapping("/nota")
 public class NotaController {
 
 	private NotaRepository notaRepository;
@@ -20,12 +24,18 @@ public class NotaController {
 		this.notaRepository = notaRepository;
 	}
 
-	@GetMapping(value="/")
+	@GetMapping
 	public List<Nota> findAll(){
 		return notaRepository.findAll();
 	}
 
-	@PostMapping(value="/")
+
+	@GetMapping("titulo/{titulo}")
+	public List<Nota> findByTitulo(@PathVariable(value="titulo") String titulo){
+		return notaRepository.findByTitulo(titulo);
+	}
+
+	@PostMapping
 	public void save(@RequestBody Nota nota){
 		notaRepository.save(nota);
 	}
--- src/main/java/br/com/fiap/notepadsaas/repository/NotaRepository.java
+++ src/main/java/br/com/fiap/notepadsaas/repository/NotaRepository.java
@@ -1,8 +1,13 @@
 package br.com.fiap.notepadsaas.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+
 import br.com.fiap.notepadsaas.model.Nota;
 
 public interface NotaRepository extends MongoRepository<Nota, String> {
+	
+	List<Nota> findByTitulo(String titulo);
 	
 }
